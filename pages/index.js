11ab5e72bde2386a83b8a5a8caaaf4f8d9a363f2
Jorge Gonçalves Junior <jorge.techinfo@gmail.com>
2020-04-08 18:07:01 -0300

    head

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Head from 'next/head';
+
 export default function Index() {
   return (
     <div className="container">
+      <Head>
+        <title>RocketsLab :: Site em construção</title>
+      </Head>
       <img className="brand" src="/images/site-logo.svg" alt="RocketsLab"/>
       <img className="slogan" src="/images/site-cga.svg" alt="Site em breve!"/>
     </div>
